c2e075a07d26f8b06332baf7399dbe12a4cc8445
Vanessa Arias <2002013@utrivieramaya.edu.mx>
2024-02-29 06:29:20 -0500

    feat: add routes to sidebar with icons

--- src/app/shared/layout/layout.component.ts
+++ src/app/shared/layout/layout.component.ts
@@ -3,17 +3,48 @@ import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatSidenavModule} from '@angular/material/sidenav';
-import { Conditional } from '@angular/compiler';
+import {MatListModule} from '@angular/material/list';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [MatToolbarModule, MatButtonModule, MatIconModule, MatSidenavModule],
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule, MatSidenavModule, MatListModule, RouterModule],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss'
 })
 export class LayoutComponent {
   opened: boolean = true;
+
+public links =[
+  {
+    icon:'restaurant_menu',
+    label:'Lista de comida',
+    routerLink:'food/food-list'
+  },
+  {
+    icon:'lunch_dining',
+    label:'Crear nueva comida',
+    routerLink:'food/form'
+  },
+  {
+    icon:'restaurant_menu',
+    label:'Lista de comida',
+    routerLink:'food/food-list'
+  },
+  {
+    icon:'restaurant_menu',
+    label:'Lista de comida',
+    routerLink:'food/food-list'
+  },
+  {
+    icon:'restaurant_menu',
+    label:'Lista de comida',
+    routerLink:'food/food-list'
+  },
+]
+
+
 
       public open():void {
 
