d977faeaffbc2b62be5d74815575c47f1de22598
Vanessa Arias <2002013@utrivieramaya.edu.mx>
2024-02-29 06:28:17 -0500

    fix: change changes from food to food list

--- src/app/foods/food-list/food-list.component.ts
+++ src/app/foods/food-list/food-list.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { FoodService } from '../shared/food.service';
+import { Food } from '../shared/food.model';
+import { log } from 'console';
+import { FoodComponent } from '../food/food.component';
 @Component({
   selector: 'app-food-list',
   standalone: true,
-  imports: [],
+  imports: [FoodComponent],
   templateUrl: './food-list.component.html',
   styleUrl: './food-list.component.scss'
 })
-export class FoodListComponent {
+
+
+export class FoodListComponent implements OnInit {
+
+  data:Food[]= [];
+  constructor(public serviceFood:FoodService) {}
+
+  ngOnInit(): void {
+    this.data = this.serviceFood.getAllFoods();
+    console.log (this.data);
+  }
 
 }
