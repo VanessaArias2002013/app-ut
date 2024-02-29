f0abaa8df6c667dbaebd635ad495d653fb9bc33d
Vanessa Arias <2002013@utrivieramaya.edu.mx>
2024-02-29 03:54:34 -0500

    feat: create foods route for add food list route

---
+++ src/app/foods/foods.routes.ts
@@ -0,0 +1,11 @@
+
+import { Routes } from '@angular/router';
+import { FoodListComponent } from './food-list/food-list.component';
+
+
+
+const routes: Routes = [
+    { path: 'food-list', component: FoodListComponent },
+];
+
+export default routes;
