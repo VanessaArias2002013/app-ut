1b5263459eae2aeff70e5575a89cfa000f7711fc
Vanessa Arias <2002013@utrivieramaya.edu.mx>
2024-03-05 18:36:52 -0500

    feat: add form validations

--- src/app/foods/form-food/form-food.component.ts
+++ src/app/foods/form-food/form-food.component.ts
@@ -1,32 +1,90 @@
 import { Component } from '@angular/core';
-import { FormControl, ReactiveFormsModule, Validators} from '@angular/forms';
-import {MatIconModule} from '@angular/material/icon';
-import {MatInputModule} from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatButtonModule} from '@angular/material/button';
+import {
+  FormBuilder,
+  FormControl,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import {MatSelectModule} from '@angular/material/select';
+import { Food } from '../shared/food.model';
+import { FoodService } from '../shared/food.service';
 
 @Component({
   selector: 'app-form-food',
   standalone: true,
-  imports: [ReactiveFormsModule, MatIconModule,MatInputModule, MatFormFieldModule, MatButtonModule ],
+  imports: [
+    ReactiveFormsModule,
+    MatIconModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatSelectModule,
+  ],
   templateUrl: './form-food.component.html',
-  styleUrl: './form-food.component.scss'
+  styleUrl: './form-food.component.scss',
 })
 export class FormFoodComponent {
-  name = new FormControl([ Validators.maxLength(20), Validators.required ]);
+  /*    id:number;
+    name:string;
+    description:string;
+    image:string;
+    category:string;
+    price:number; []*/
 
-  public showValue(){
-    if (this.name.hasError('required')){
-      return 'You must enter a value';
-    } else if (this.name.hasError('maxLength')){
-      return 'Minimo 20 caracteres'
+  form = this.formBuilder.group({
+    name: ['', [Validators.required]],
+    description: ['', [Validators.required]],
+    image: ['', [Validators.required]],
+    category: ['', [Validators.required]],
+    price: ['', [Validators.required, Validators.min(2)]],
+  });
+  constructor(private formBuilder: FormBuilder, public serviceFood:FoodService) {}
+
+
+  public sendData(){
+    if (this.form.status == 'VALID'){
+      if(
+        this.name?.value &&
+        this.description?.value &&
+        this.image?.value &&
+        this.category?.value &&
+        this.price?.value
+
+      ){
+        let priceNumber = Number(this.price.value);
+        let comida: Food = {
+          name: this.name?.value,
+          description: this.description?.value,
+          category: this.category?.value,
+         image: this.image?.value,
+          price: priceNumber
+        };
+        console.log(comida);
+        this.serviceFood.addFood(comida);
+
+      }
     }
-    return 'Error';
+  }
+  get name() {
+    return this.form.get('name');
+  }
+  get description() {
+    return this.form.get('description');
+  }
+  get image() {
+    return this.form.get('image');
+  }
+  get category() {
+    return this.form.get('category');
+  }
+  get price() {
+    return this.form.get('price');
   }
 
-  public changeValue():void{
-  //  this.name.setValue('Pizza de Pepperoni')
-  }
 
-
+ 
 }
