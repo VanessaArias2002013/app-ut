5ad8d33871fca922fc97a3c441065fc55002315a
Vanessa Arias <2002013@utrivieramaya.edu.mx>
2024-03-05 18:35:53 -0500

    feat: add function to add, update and delete food

--- src/app/foods/shared/food.service.ts
+++ src/app/foods/shared/food.service.ts
@@ -2,83 +2,111 @@ import { Injectable } from '@angular/core';
 import { Food } from './food.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class FoodService {
-
-
-  menu:Food []= [
+  menu: Food[] = [
     {
-      id:1,
-      name:'pizza',
-      description:'carnes',
-      category:'food',
-      image:'https://th.bing.com/th/id/OIP.1ebAb8OKVl6BHm-Ju7cf5AHaE7?rs=1&pid=ImgDetMain',
-      price:300,
+      id: 1,
+      name: 'pizza',
+      description: 'carnes',
+      category: 'food',
+      image:
+        'https://th.bing.com/th/id/OIP.1ebAb8OKVl6BHm-Ju7cf5AHaE7?rs=1&pid=ImgDetMain',
+      price: 300,
     },
     {
-      id:2,
-      name:'pizza',
-      description:'carnes',
-      category:'food',
-      image:'https://th.bing.com/th/id/OIP.1ebAb8OKVl6BHm-Ju7cf5AHaE7?rs=1&pid=ImgDetMain',
-      price:300,
+      id: 2,
+      name: 'pizza',
+      description: 'carnes',
+      category: 'food',
+      image:
+        'https://th.bing.com/th/id/OIP.1ebAb8OKVl6BHm-Ju7cf5AHaE7?rs=1&pid=ImgDetMain',
+      price: 300,
     },
     {
-      id:3,
-      name:'pizza',
-      description:'carnes',
-      category:'food',
-      image:'https://th.bing.com/th/id/OIP.1ebAb8OKVl6BHm-Ju7cf5AHaE7?rs=1&pid=ImgDetMain',
-      price:300,
+      id: 3,
+      name: 'pizza',
+      description: 'carnes',
+      category: 'food',
+      image:
+        'https://th.bing.com/th/id/OIP.1ebAb8OKVl6BHm-Ju7cf5AHaE7?rs=1&pid=ImgDetMain',
+      price: 300,
     },
     {
-      id:4,
-      name:'pizza',
-      description:'carnes',
-      category:'food',
-      image:'https://th.bing.com/th/id/OIP.1ebAb8OKVl6BHm-Ju7cf5AHaE7?rs=1&pid=ImgDetMain',
-      price:300,
+      id: 4,
+      name: 'pizza',
+      description: 'carnes',
+      category: 'food',
+      image:
+        'https://th.bing.com/th/id/OIP.1ebAb8OKVl6BHm-Ju7cf5AHaE7?rs=1&pid=ImgDetMain',
+      price: 300,
     },
     {
-      id:5,
-      name:'pizza',
-      description:'carnes',
-      category:'food',
-      image:'https://th.bing.com/th/id/OIP.1ebAb8OKVl6BHm-Ju7cf5AHaE7?rs=1&pid=ImgDetMain',
-      price:300,
+      id: 5,
+      name: 'pizza',
+      description: 'carnes',
+      category: 'food',
+      image:
+        'https://th.bing.com/th/id/OIP.1ebAb8OKVl6BHm-Ju7cf5AHaE7?rs=1&pid=ImgDetMain',
+      price: 300,
     },
     {
-      id:6,
-      name:'pizza',
-      description:'carnes',
-      category:'food',
-      image:'https://th.bing.com/th/id/OIP.1ebAb8OKVl6BHm-Ju7cf5AHaE7?rs=1&pid=ImgDetMain',
-      price:300,
+      id: 6,
+      name: 'pizza',
+      description: 'carnes',
+      category: 'food',
+      image:
+        'https://th.bing.com/th/id/OIP.1ebAb8OKVl6BHm-Ju7cf5AHaE7?rs=1&pid=ImgDetMain',
+      price: 300,
     },
     {
-      id:7,
-      name:'pizza',
-      description:'carnes',
-      category:'food',
-      image:'https://th.bing.com/th/id/OIP.1ebAb8OKVl6BHm-Ju7cf5AHaE7?rs=1&pid=ImgDetMain',
-      price:300,
+      id: 7,
+      name: 'pizza',
+      description: 'carnes',
+      category: 'food',
+      image:
+        'https://th.bing.com/th/id/OIP.1ebAb8OKVl6BHm-Ju7cf5AHaE7?rs=1&pid=ImgDetMain',
+      price: 300,
     },
     {
-      id:8,
-      name:'pizza',
-      description:'carnes',
-      category:'food',
-      image:'https://th.bing.com/th/id/OIP.1ebAb8OKVl6BHm-Ju7cf5AHaE7?rs=1&pid=ImgDetMain',
-      price:300,
+      id: 8,
+      name: 'pizza',
+      description: 'carnes',
+      category: 'food',
+      image:
+        'https://th.bing.com/th/id/OIP.1ebAb8OKVl6BHm-Ju7cf5AHaE7?rs=1&pid=ImgDetMain',
+      price: 300,
     },
-    
-  ]
+  ];
 
   constructor() {}
 
-
-   public getAllFoods():Food[]{
+  public getAllFoods(): Food[] {
     return this.menu;
-   }
+  }
+
+  //añadir foods
+  public addFood(food: Food) {
+    this.menu.push(food);
+  }
+
+  //actualizar food
+  public update(newFood: Food) {
+    this.menu.forEach((food, index) => {
+      if (food.id == newFood.id) {
+        food = newFood;
+      }
+    })
+  }
+
+
+  //Eliminar comida
+  public deleteFood(deleteFood:Food){
+    this.menu.forEach((food, index) => {
+      if (food.id == deleteFood.id){
+        this.menu.splice(index,1);
+      }
+    })
+  }
 }
