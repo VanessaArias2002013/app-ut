6ce76c27bc9fd0fd8f4d653c0281bd153c5613f2
Vanessa Arias <2002013@utrivieramaya.edu.mx>
2024-03-15 16:28:08 -0500

    feat: add funtions to delete and update foods

--- src/app/foods/food/food.component.ts
+++ src/app/foods/food/food.component.ts
@@ -3,13 +3,23 @@ import {MatButtonModule} from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card';
 import{Food} from '../shared/food.model';
 import { CurrencyPipe, TitleCasePipe } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { FoodService } from '../shared/food.service';
 @Component({
   selector: 'app-food',
   standalone: true,
-  imports: [MatCardModule, MatButtonModule, CurrencyPipe, TitleCasePipe],
+  imports: [MatCardModule, MatButtonModule, CurrencyPipe, TitleCasePipe, RouterModule],
   templateUrl: './food.component.html',
   styleUrl: './food.component.scss'
 })
 export class FoodComponent {
   @Input() food?:Food;
+
+  constructor (public foodService:FoodService){
+
+  }
+
+  public deleteFood(food:Food){
+    this.foodService.deleteFood(food);
+  }
 }
--- src/app/foods/form-food/form-food.component.ts
+++ src/app/foods/form-food/form-food.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import {
   FormBuilder,
   FormControl,
@@ -12,6 +12,7 @@ import { MatButtonModule } from '@angular/material/button';
 import {MatSelectModule} from '@angular/material/select';
 import { Food } from '../shared/food.model';
 import { FoodService } from '../shared/food.service';
+import { ActivatedRoute, Router, Routes } from '@angular/router';
 
 @Component({
   selector: 'app-form-food',
@@ -23,17 +24,13 @@ import { FoodService } from '../shared/food.service';
     MatFormFieldModule,
     MatButtonModule,
     MatSelectModule,
+    
   ],
   templateUrl: './form-food.component.html',
   styleUrl: './form-food.component.scss',
 })
-export class FormFoodComponent {
-  /*    id:number;
-    name:string;
-    description:string;
-    image:string;
-    category:string;
-    price:number; []*/
+export class FormFoodComponent implements OnInit{
+ 
 
   form = this.formBuilder.group({
     name: ['', [Validators.required]],
@@ -42,8 +39,67 @@ export class FormFoodComponent {
     category: ['', [Validators.required]],
     price: ['', [Validators.required, Validators.min(2)]],
   });
-  constructor(private formBuilder: FormBuilder, public serviceFood:FoodService) {}
+  constructor(private formBuilder: FormBuilder, public serviceFood:FoodService, public router:Router) {}
 
+  foodId:number = -1;
+  edit:boolean = false;
+  activeRoute:ActivatedRoute = inject(ActivatedRoute);
+  food?:Food = {
+  name:'',
+  description:'',
+  category:'',
+  image:'',
+  price:0
+  }
+
+
+
+  ngOnInit(): void {
+    if(this.activeRoute.snapshot.params['id']){
+      this.edit = true;
+      this.foodId = Number(this.activeRoute.snapshot.params['id'])
+      console.log(this.foodId);
+      this.food = this.serviceFood.getOne(this.foodId);
+      if(this.food){
+        this.form.patchValue({
+          name:this.food.name,
+          description:this.food.description,
+          image:this.food.image,
+          category:this.food.category,
+          price:this.food.price.toString()
+
+
+        })
+
+      }
+    }
+  }
+
+  public updateData(){
+    if (this.form.status == 'VALID'){
+      if(
+        this.name?.value &&
+        this.description?.value &&
+        this.image?.value &&
+        this.category?.value &&
+        this.price?.value
+
+      ){
+        let priceNumber = Number(this.price.value);
+        let comida: Food = {
+          id:this.foodId,
+          name: this.name?.value,
+          description: this.description?.value,
+          category: this.category?.value,
+          image: this.image?.value,
+          price: priceNumber
+        };
+        console.log(comida);
+        this.serviceFood.updateFood(comida);
+        this.router.navigate(['food/food-list'])
+      }
+    }
+  }
 
   public sendData(){
     if (this.form.status == 'VALID'){
@@ -65,7 +121,7 @@ export class FormFoodComponent {
         };
         console.log(comida);
         this.serviceFood.addFood(comida);
-
+        this.router.navigate(['food/food-list'])
       }
     }
   }
