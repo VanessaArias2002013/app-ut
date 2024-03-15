bc7c44956ed9e17820aa3d18f8dfbe1f042a35be
Vanessa Arias <2002013@utrivieramaya.edu.mx>
2024-03-15 16:27:04 -0500

    feat: add route to update and details

--- src/app/foods/foods.routes.ts
+++ src/app/foods/foods.routes.ts
@@ -1,14 +1,30 @@
 import { Routes } from '@angular/router';
 import { FoodListComponent } from './food-list/food-list.component';
 import { FormFoodComponent } from './form-food/form-food.component';
+import { DetailsFoodComponent } from './details-food/details-food.component';
 
 export default [
   {
      path: 'food-list',
-      component: FoodListComponent
+      component: FoodListComponent,
+      title: 'Lista de comida'
     },
+    
     {
     path: 'form',
-    component: FormFoodComponent
-    }
+    component: FormFoodComponent,
+    title:'Crear comida'
+    },
+
+    {
+      path: 'form/:id',
+       component: FormFoodComponent,
+       title: 'Actualizar comida'
+     },
+
+     {
+      path: 'details-food/:id',
+       component: DetailsFoodComponent,
+       title: 'Detalles de la comida'
+     },
     ]
