8087f513c765e7ac3cfe43d5ee0f8c2c07082c33
Vanessa Arias <2002013@utrivieramaya.edu.mx>
2024-04-18 07:49:01 -0500

    feat: add button delete and dialog

--- src/app/foods/food/food.component.ts
+++ src/app/foods/food/food.component.ts
@@ -1,25 +1,51 @@
 import { Component, Input } from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card';
-import{Food} from '../shared/food.model';
+import { MatIcon } from '@angular/material/icon';
 import { CurrencyPipe, TitleCasePipe } from '@angular/common';
-import { RouterModule } from '@angular/router';
+import { Food } from '../shared/food.model';
 import { FoodService } from '../shared/food.service';
+import {MatDialog} from '@angular/material/dialog';
+import { DeleteDialogComponent } from '../../shared/components/delete-dialog/delete-dialog.component';
+import { RouterModule } from '@angular/router';
+import { Output, EventEmitter } from '@angular/core';
+
 @Component({
   selector: 'app-food',
   standalone: true,
-  imports: [MatCardModule, MatButtonModule, CurrencyPipe, TitleCasePipe, RouterModule],
+  imports: [MatButtonModule, MatCardModule,TitleCasePipe, CurrencyPipe,MatIcon,RouterModule],
   templateUrl: './food.component.html',
   styleUrl: './food.component.scss'
 })
 export class FoodComponent {
-  @Input() food?:Food;
-
-  constructor (public foodService:FoodService){
-
+  @Input() food?:Food; 
+  @Output() eventDeleteItem = new EventEmitter<boolean>;
+  constructor (public serviceFood:FoodService, public dialog:MatDialog){
+    
   }
 
-  public deleteFood(food:Food){
-    this.foodService.deleteFood(food);
+  openDialog(deleteFood:Food) {
+    const dialogRef = this.dialog.open(DeleteDialogComponent,{
+      data:deleteFood
+    });
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(`Dialog result: ${result}`);
+      if (result == true) {
+        this.delete(deleteFood)
+      } 
+    });
   }
+
+  public delete(food:Food){
+    this.serviceFood.deleteFood(food).subscribe({
+      next:()=>  console.log('Se esta eliminando'),
+      error:(e) => console.error(e),
+      complete:()=> this.deleteItemEvent(true),
+    })
+  }
+
+  public deleteItemEvent(value:boolean){
+    this.eventDeleteItem.emit(value);
+  }
+
 }
