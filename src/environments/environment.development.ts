a199e3e89cce085506e88e9f0598b9f52c56f422
Vanessa Arias <2002013@utrivieramaya.edu.mx>
2024-04-18 22:15:02 -0500

    feat: add links to the render

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production:true,
-    API_URL:"http://localhost:3000/"
+    API_URL:"https://backend-menu-0m8z.onrender.com/"
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,1 +1,4 @@
-export const environment = {};
+export const environment = {
+    production:true,
+    API_URL:"https://backend-menu-0m8z.onrender.com/"
+};
